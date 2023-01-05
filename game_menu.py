be3e3ee727fdeb757b6dc4cdb06fbd65ec7bded4
KFN001 <fedotovkirill4000@gmail.com>
2023-01-05 17:55:39 +0400

    almost done menu

--- game_menu.py
+++ game_menu.py
@@ -2,25 +2,54 @@ import pygame
 import pygame_menu
 from pygame_menu import Theme
 from pygame import mixer
+import sqlite3
 
 
 def draw_background():
-    print('drawing')
+    pic = pygame_menu.baseimage.BaseImage('data/planes/f-4c.png')
 
 
-pygame.init()
-pygame.display.set_caption('Sky Bandits')
-mixer.music.load('data/music/arcade_theme.mp3')
-mixer.music.set_volume(0.2)
-mixer.music.play()
-background = pygame_menu.baseimage.BaseImage('data/hangar.png')
-sc_size = width, height = 1200, 600
-surface = pygame.display.set_mode(sc_size)
-my_theme = Theme(background_color=(0, 0, 0, 0), title_background_color=(4, 47, 126),
-                 title_font_shadow=True, title_font=pygame_menu.font.FONT_8BIT,
-                 widget_padding=25, widget_font=pygame_menu.font.FONT_8BIT,
-                 title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_ADAPTIVE,
-                 widget_font_color=pygame.Color('white'))
-my_theme.background_color = background
-menu = pygame_menu.Menu('Sky Bandits', width, height, theme=my_theme)
-menu.mainloop(surface, fps_limit=60, bgfun=draw_background)
+def buy_plane():
+    pass
+
+
+def game(num):
+    pass
+
+
+def start():
+    pygame.init()
+    planes = []
+    con = sqlite3.connect('planes.db')
+    cur = con.cursor()
+    result = cur.execute("""SELECT model, image from planes""").fetchall()
+    for i, j in enumerate(result):
+        planes.append((j[0], i))
+    print(planes)
+    money = 100
+    con.close()
+    pygame.display.set_caption('Sky Bandits')
+    mixer.music.load('data/music/arcade_theme.mp3')
+    mixer.music.set_volume(0.2)
+    mixer.music.play(-1)
+    background = pygame_menu.baseimage.BaseImage('data/hangar.png')
+    sc_size = width, height = 1200, 600
+    surface = pygame.display.set_mode(sc_size)
+    my_theme = Theme(background_color=(0, 0, 0, 0), title_background_color=(4, 47, 126),
+                     title_font_shadow=True, title_font=pygame_menu.font.FONT_8BIT,
+                     widget_padding=25, widget_font=pygame_menu.font.FONT_8BIT,
+                     title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_ADAPTIVE,
+                     widget_font_color=pygame.Color('white'))
+    my_theme.background_color = background
+    menu = pygame_menu.Menu('Sky Bandits', width, height, theme=my_theme)
+    menu.add.label(f'Money {money}', align=pygame_menu.locals.ALIGN_RIGHT)
+    menu.add.selector('Select Plane', planes, align=pygame_menu.locals.ALIGN_RIGHT)
+    menu.add.button('BUY Plane', buy_plane, align=pygame_menu.locals.ALIGN_RIGHT)
+    menu.add.button('Play Sturm', game(1), align=pygame_menu.locals.ALIGN_LEFT)
+    menu.add.button('Play War', game(2), align=pygame_menu.locals.ALIGN_LEFT)
+    menu.add.button('Play Defend', game(3), align=pygame_menu.locals.ALIGN_LEFT)
+    menu.add.button('Play onslaught', game(4), align=pygame_menu.locals.ALIGN_LEFT)
+    menu.mainloop(surface, fps_limit=60, bgfun=draw_background)
+
+
+start()
--- start_menu.py
+++ start_menu.py
@@ -2,12 +2,7 @@ import pygame_menu
 from pygame_menu import sound, Theme
 import pygame
 from pygame import mixer
-
-'''
-engine = sound.Sound()
-engine.set_sound(sound.SOUND_TYPE_CLICK_MOUSE, 'data/music/button.wav')
-engine.set_sound(sound.SOUND_TYPE_OPEN_MENU, 'data/music/button.wav')
-'''
+import game_menu
 
 
 def check_player(name):
@@ -18,7 +13,8 @@ def check_player(name):
 def start_game():
     if check_player(menu.get_input_data()):
         mixer.music.stop()
-
+        menu.close()
+        game_menu.start()
     else:
         menu.reset_value()
 
@@ -29,8 +25,6 @@ mixer.music.load('data/music/theme.mp3')
 mixer.music.set_volume(0.2)
 mixer.music.play()
 background = pygame_menu.baseimage.BaseImage('data/background.jpg')
-
-
 sc_size = width, height = 1200, 600
 surface = pygame.display.set_mode(sc_size)
 my_theme = Theme(background_color=(0, 0, 0, 0), title_background_color=(4, 47, 126),
@@ -41,14 +35,14 @@ my_theme = Theme(background_color=(0, 0, 0, 0), title_background_color=(4, 47, 1
 my_theme.background_color = background
 menu = pygame_menu.Menu('Sky Bandits', width, height, theme=my_theme)
 menu.add.label('Login or Sign up', font_size=20)
-menu.add.text_input('Name:', default='Your name', font_size=20)
-menu.add.text_input('Password:', default='Your password', font_size=20)
+menu.add.text_input('', default='Your name', font_size=20)
+menu.add.text_input('', default='Your password', font_size=20)
 menu.add.button('Play', start_game, font_size=40)
 menu.add.button('Quit', pygame_menu.events.EXIT, font_size=30)
 menu.center_content()
 menu.add.label('Game by KFN001', align=pygame_menu.locals.ALIGN_LEFT,
                font_color=pygame.Color('grey'), font_size=8)
-'''
-menu.set_sound(engine)
-'''
+engine = sound.Sound(-1)
+engine.set_sound(pygame_menu.sound.SOUND_TYPE_WIDGET_SELECTION, 'data/music/button.wav')
+menu.set_sound(engine, recursive=True)
 menu.mainloop(surface, fps_limit=60)
