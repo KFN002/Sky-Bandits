18792c4462aa047ac5b9b7d7b2f7a0cddd7fece4
KFN001 <fedotovkirill4000@gmail.com>
2023-01-26 19:32:59 +0300

    doing game 0.99979 -adding second gameplay level -fixed offline bag -finished 1st gameplay level -fixed classes: enemybase, bullet, bomb, player -added sounds -making leaderboard -fixed balance -fully done 1st level -added structures destruction -added leaderboard -fixed fonts -fixed collision -added classes optimization -optimized levels

--- data_master.py
+++ data_master.py
@@ -12,13 +12,6 @@ import game_menu
 name = 'Sky Bandits: players'
 sheet = client.create(name)
 sheet.share('fedotovkirill4000@gmail.com', perm_type='user', role='writer')
-sheet.row_values(3)
-sheet.col_values(2)
-sheet.cell(2, 2).value()
-sheet = client.open(name).sheet1
-sheet.update_cell(1, 1, "Ivan")
-print(sheet.append_row(['Mike'], 2))
-print(sheet.get_all_values())
 '''
 
 
