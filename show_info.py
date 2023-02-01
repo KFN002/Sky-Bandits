0694b662dc1e5a2bda07806aa9e0500a6c2a77d5
KFN001 <fedotovkirill4000@gmail.com>
2023-02-01 13:01:08 +0300

    doing game 1.0 -adding easter eggs

--- show_info.py
+++ show_info.py
@@ -1,5 +1,6 @@
 import sqlite3
 import webbrowser
+from random import choice
 
 
 def redirect(plane):   # запуск браузра с инфой: ссылка в бд
@@ -8,3 +9,6 @@ def redirect(plane):   # запуск браузра с инфой: ссылка
     web_page = cur.execute(f"""SELECT web_page FROM planes WHERE model = '{plane[0][0]}'""").fetchone()
     con.close()
     webbrowser.open(web_page[0])
+    if choice([True, False, False, False, False, False, False, False]):
+        webbrowser.open('https://www.youtube.com/watch?v=VE03Lqm3nbI')
+
